07f8b258ccf67d284b76d82dadc6fe496c7312c9
Tamila57 <pereptamila57@gmail.com>
2024-05-27 02:02:27 +0300

    add orders for each product

--- JavaScript/shop.js
+++ JavaScript/shop.js
@@ -1,57 +1,62 @@
 document.addEventListener("partialsLoaded", () => {
-    import("./burgerMenu.js");
-  });
+  import("./burgerMenu.js");
+});
 
 const products = [
-    {
-        id: '1',
-        title: 'Eternal Circles Ring',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!',
-        price: 49.99,
-        image: 'img/MagicEternalCirclesRing.jpg"'
-    },
-    {
-        id: '2',
-        title: 'Magic Forest Ring',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!',
-        price: 50,
-        image: 'img/MAgicForestRing.jpg'
-    },
-    {
-        id: '3',
-        title: 'Magic Lion Flash Ring',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!',
-        price: 75,
-        image: 'img/MagicLionFlashRing.jpg'
-    },
-    {
-        id: '4',
-        title: 'Magic Sun Ring',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!',
-        price: 55,
-        image: 'img/MagicSunRing.jpg'
-    },
-    {
-        id: '5',
-        title: 'Magic Wood Ring',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!',
-        price: 69.99,
-        image: 'img/MagicWoodRing.jpg'
-    },
-    {
-        id: '6',
-        title: 'Magic Winter Ring',
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!',
-        price: 99.99,
-        image: 'img/MagicWinterRing.jpg'
-    },
-    
+  {
+    id: "1",
+    title: "Eternal Circles Ring",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!",
+    price: 49.99,
+    image: 'img/MagicEternalCirclesRing.jpg"',
+  },
+  {
+    id: "2",
+    title: "Magic Forest Ring",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!",
+    price: 50,
+    image: "img/MAgicForestRing.jpg",
+  },
+  {
+    id: "3",
+    title: "Magic Lion Flash Ring",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!",
+    price: 75,
+    image: "img/MagicLionFlashRing.jpg",
+  },
+  {
+    id: "4",
+    title: "Magic Sun Ring",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!",
+    price: 55,
+    image: "img/MagicSunRing.jpg",
+  },
+  {
+    id: "5",
+    title: "Magic Wood Ring",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!",
+    price: 69.99,
+    image: "img/MagicWoodRing.jpg",
+  },
+  {
+    id: "6",
+    title: "Magic Winter Ring",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro fuga autem possimus eveniet facere, non minus magnam obcaecati, tenetur recusandae, corporis itaque suscipit. Vero illo nulla asperiores laudantium tempore porro!",
+    price: 99.99,
+    image: "img/MagicWinterRing.jpg",
+  },
 ];
 
 function renderProducts(products) {
-    let productsHtml = '';
-    for (const product of products) {
-        productsHtml += `
+  let productsHtml = "";
+  for (const product of products) {
+    productsHtml += `
         <article class="product-card">
             <img src="${product.image}" alt="${product.title}">
             <h3 class="product-card__h3">${product.title}</h3>
@@ -64,64 +69,78 @@ function renderProducts(products) {
                 </a>
             </div>
         </article>`;
-    }
-    const productsContainer = document.querySelector('.products__list');
-    productsContainer.innerHTML = productsHtml;
+  }
+  const productsContainer = document.querySelector(".products__list");
+  productsContainer.innerHTML = productsHtml;
 }
 
-// function handleOrderButtonClick(event) {
-//     event.preventDefault();
-//     const productId = event.target.dataset.productId;
-//     const product = products.find(item => item.id === productId);
-//     if (product) {
-        
-//         console.log(`Ordering ${product.title}`);
-//     }
-// }
-
 function createOrderForm(product) {
-    // Створення форми для замовлення
-    const form = document.createElement('form');
-    form.innerHTML = `
-        <h2>Order ${product.title}</h2>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+  const form = document.createElement("form");
+  form.setAttribute("action", "https://formspree.io/f/xzbnqopz");
+  form.setAttribute("method", "POST");
+
+  form.innerHTML = `
+  <div class="form-status"></div>
+        <h2 class="form__title">Order ${product.title}</h2>
+        <input type="text" id="name" name="name" 
+        placeholder="Enter your name*"
+        required class="form__item">
+        <input type="email" id="email" name="email"
+        placeholder="Enter your e-mail*"
+         required class="form__item">
+         <input type="text" id="quantity" name="quantity" 
+        placeholder="Enter quantity of product*"
+        required class="form__item">
         <input type="hidden" name="product_id" value="${product.id}">
-        <button type="submit">Submit</button>
+        <input type="hidden" name="product_name" value="${product.title}">
+        <button type="submit" class="form__button">Submit</button>
+        
     `;
 
-    // Обробник події для відправки форми замовлення
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const formDataObject = Object.fromEntries(formData.entries());
-        console.log('Order:', formDataObject); // Тут ви можете відправити дані форми на сервер або виконати інші дії з ними
-        // Очищення форми після відправки
-        form.remove();
-    });
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const formDataObject = Object.fromEntries(formData.entries());
 
-    return form;
+    try {
+      const status = document.querySelector(".form-status");
+      const response = await fetch(form.getAttribute("action"), {
+        method: form.getAttribute("method"),
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.remove();
+      } else {
+        status.innerHTML = "Form submission failed!";
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  });
+
+  return form;
 }
 
 function handleOrderButtonClick(event) {
-    event.preventDefault();
-    const productId = event.target.dataset.productId;
-    const product = products.find(item => item.id === productId);
-    if (product) {
-        // Створення та відображення форми замовлення
-        const orderForm = createOrderForm(product);
-        document.body.appendChild(orderForm);
-    }
+  event.preventDefault();
+  const productId = event.target.dataset.productId;
+  const product = products.find((item) => item.id === productId);
+  if (product) {
+    const orderForm = createOrderForm(product);
+    document.body.appendChild(orderForm);
+  }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  renderProducts(products);
 
-document.addEventListener('DOMContentLoaded', () => {
-    renderProducts(products);
-
-    const orderButtons = document.querySelectorAll('.product-card__buttons-buy');
-    orderButtons.forEach(button => {
-        button.addEventListener('click', handleOrderButtonClick);
-    });
+  const orderButtons = document.querySelectorAll(".product-card__buttons-buy");
+  orderButtons.forEach((button) => {
+    button.addEventListener("click", handleOrderButtonClick);
+  });
 });
